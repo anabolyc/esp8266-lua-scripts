c6271a388f1cb67978db86228b0ebeef58a7de32
andrey.malyshenko <andrey.malyshenko@gmail.com>
2017-04-22 19:54:08 +0200

    minor changes

--- panda-lamp/init.lua
+++ panda-lamp/init.lua
@@ -4,11 +4,11 @@ local _pass = "9263777101";
 local LED_TMR_ID = 1;
 local WEB_TMR_ID = 2;
 
-local PIN_SW  = 6;  -- GPIO_01
+local PIN_SW  = 0;  -- GPIO_16
 local PIN_TOUCH = 5; -- GPIO_14
 local PIN_LED = 4;
 
-local DEBUG = true;
+local DEBUG = false;
 local ext = (DEBUG == true and ".lua" or ".lc");
 local LC_LED_BLINK = 'led-blink' .. ext;
 local LC_FIFI_CONN = 'wifi-connect' .. ext;
@@ -16,11 +16,15 @@ local LC_WEB_SRV   = 'web-srv' .. ext;
 local LC_TCH_SENS  = 'touch-sensor' .. ext;
 local LC_WEB_SRV_FS= 'web-srv-fs' .. ext;
 
-local led_blink = dofile(LC_LED_BLINK);
-led_blink.start(PIN_LED, LED_TMR_ID);
+if (DEBUG) then
+    local led_blink = dofile(LC_LED_BLINK);
+    led_blink.start(PIN_LED, LED_TMR_ID);
+end
 
-local touch_1 = dofile(LC_TCH_SENS);
-touch_1.start(PIN_TOUCH, PIN_SW);
+if (DEBUG) then
+    local touch_1 = dofile(LC_TCH_SENS);
+    touch_1.start(PIN_TOUCH, PIN_SW);
+end
 
 local wifi = dofile(LC_FIFI_CONN);
 local web_srv = dofile(LC_WEB_SRV);
@@ -28,6 +32,6 @@ local web_srv_fs = dofile(LC_WEB_SRV_FS);
 
 wifi.start(_ssid, _pass, function()
     web_srv_fs.start(WEB_TMR_ID)
-    web_srv.start(8080, PIN_SW, web_srv_fs);
+    web_srv.start(80, PIN_SW, web_srv_fs);
     print("wifi connected");
 end);
